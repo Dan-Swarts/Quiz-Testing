b65de890763d751ff575ffaa114e999c469a697f
Daniel <danstraws@gmail.com>
2024-12-23 15:45:25 -0600

    fixed path to client dist

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,13 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "../client/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-// }
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
