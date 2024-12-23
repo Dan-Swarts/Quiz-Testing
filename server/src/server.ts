715394d0cce6e3d440889de5914cc7abe689d2ac
Daniel <danstraws@gmail.com>
2024-12-23 14:43:23 -0600

    fixed __dirname path issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
-import express from 'express';
-import path from 'node:path';
+import express from "express";
+import path from "node:path";
 
-import db from './config/connection.js';
-import routes from './routes/index.js';
+import db from "./config/connection.js";
+import routes from "./routes/index.js";
+
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 await db();
 
@@ -13,11 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
 
